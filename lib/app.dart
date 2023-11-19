b9547b15f64a065d22d85f162d8e26d0f8b7c091
onishi taisei <taisei.developer@gmail.com>
2023-11-20 02:04:50 +0900

    🚀 feat: Themeの設定追加

--- lib/app.dart
+++ lib/app.dart
@@ -1,26 +1,29 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:nost/core/i18n/strings.g.dart';
+import 'package:nost/core/navigation/router/router.dart';
+import 'package:nost/core/res/theme.dart';
 
-final class App extends StatelessWidget {
+final class App extends ConsumerWidget {
   const App({super.key});
 
   static const appName = 'Nost';
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  Widget build(BuildContext context, WidgetRef ref) {
+    final router = ref.watch(routerProvider);
+    return MaterialApp.router(
       title: appName,
       debugShowCheckedModeBanner: false,
+      routerConfig: router,
       locale: TranslationProvider.of(context).flutterLocale,
       builder: DevicePreview.appBuilder,
       supportedLocales: AppLocaleUtils.supportedLocales,
       localizationsDelegates: GlobalMaterialLocalizations.delegates,
-      theme: ThemeData(
-        useMaterial3: true,
-      ),
-      home: const HomePage(),
+      theme: getLightTheme(),
+      darkTheme: getDarkTheme(),
     );
   }
 }
---
+++ lib/core/res/theme.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+ThemeData getLightTheme() {
+  final base = ThemeData(
+    useMaterial3: true,
+  );
+  return base.copyWith();
+}
+
+ThemeData getDarkTheme() {
+  final base = ThemeData(
+    useMaterial3: true,
+  );
+  return base.copyWith();
+}
