77b3b48cd8d7c4771baf1b9406a6f1cacf9ab68a
onishi taisei <taisei.developer@gmail.com>
2023-11-19 22:08:37 +0900

    🔧 chore: device_previewの設定を追加

---
+++ lib/app.dart
@@ -0,0 +1,38 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/material.dart';
+
+final class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Nost',
+      debugShowCheckedModeBanner: false,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('テスト'),
+          ],
+        ),
+      ),
+    );
+  }
+}
